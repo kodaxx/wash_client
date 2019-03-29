4b425135f548220c6ef3ba6a99d4a49acde3398d
Spencer Kuzara <kodaxx@gmail.com>
2019-03-28 18:52:15 -0600

    buttons test

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
-    <h1>map</h1>
+    <video autoplay muted loop id="video">
+      <source src="../assets/gotomoto_menu1.mp4" type="video/mp4">
+    </video>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Map',
-  props: {
-    msg: String
-  }
+  name: 'Map'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#video {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  max-width: 100%;
+  max-height: 100%;
+}
 </style>
